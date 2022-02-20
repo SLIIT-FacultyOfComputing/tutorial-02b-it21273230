a05fb0b5dbb4481e1b2d6f2bdb5401f250ab63eb
it21273230 <99288130+it21273230@users.noreply.github.com>
2022-02-20 20:32:55 +0000

    Exercise 4 finished

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,18 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int n)
+{
+  int fac=1, r;
+  for(r=n; r>=1; r--)
+  {
+    fac=fac*r;
+  }
+  return fac;  
+}
+
+long nCr(int n, int r)
+{
+  return Factorial(n)/
+  Factorial(r)*Factorial(n-r);
+}
